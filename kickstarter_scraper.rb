fc4a8e7a392deb94059f92f27189130a33f9faf0
Meghan Flynn <mflynn333@gmail.com>
2020-01-28 22:53:35 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,8 +1,8 @@
-# require libraries/modules here
+require 'Nokogiri'
 
 def create_project_hash
   # This just opens a file and reads it into a variable
-html = File.read('fixtures/kickstarter.html')
+  html = File.read('fixtures/kickstarter.html')
  
-kickstarter = Nokogiri::HTML(html)
+  kickstarter = Nokogiri::HTML(html)
 end
